d8f1ebf2a83affc6b5021e3f8ec2b2e5c66b7500
Andrés Contreras Guillén <me@ancongui.com>
2025-10-29 16:32:32 +0100

    feat: initial commit for lib-notifications-firebase

---
+++ src/main/java/com/firefly/core/notifications/provider/firebase/config/v1/FcmConfig.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2025 Firefly Software Solutions Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.firefly.core.notifications.provider.firebase.config.v1;
+
+import com.firefly.core.notifications.provider.firebase.properties.v1.FcmProperties;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Configuration
+public class FcmConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp(FcmProperties properties) throws IOException {
+        FirebaseOptions.Builder builder = FirebaseOptions.builder();
+        if (properties.getCredentialsPath() != null && !properties.getCredentialsPath().isBlank()) {
+            try (FileInputStream serviceAccount = new FileInputStream(properties.getCredentialsPath())) {
+                builder.setCredentials(GoogleCredentials.fromStream(serviceAccount));
+            }
+        } else {
+            builder.setCredentials(GoogleCredentials.getApplicationDefault());
+        }
+        if (properties.getProjectId() != null && !properties.getProjectId().isBlank()) {
+            builder.setProjectId(properties.getProjectId());
+        }
+        FirebaseOptions options = builder.build();
+        List<FirebaseApp> apps = FirebaseApp.getApps();
+        if (apps == null || apps.isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        }
+        return apps.get(0);
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
---
+++ src/main/java/com/firefly/core/notifications/provider/firebase/core/v1/FcmPushProvider.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2025 Firefly Software Solutions Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.firefly.core.notifications.provider.firebase.core.v1;
+
+import com.firefly.core.notifications.interfaces.dtos.push.v1.PushNotificationRequest;
+import com.firefly.core.notifications.interfaces.dtos.push.v1.PushNotificationResponse;
+import com.firefly.core.notifications.interfaces.interfaces.providers.push.v1.PushProvider;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Map;
+
+@Component
+public class FcmPushProvider implements PushProvider {
+
+    @Autowired
+    private FirebaseMessaging messaging;
+
+    @Override
+    public Mono<PushNotificationResponse> sendPush(PushNotificationRequest request) {
+        return Mono.fromCallable(() -> {
+            Message.Builder builder = Message.builder();
+            if (request.getToken() != null && !request.getToken().isBlank()) {
+                builder.setToken(request.getToken());
+            }
+            Notification notification = Notification.builder()
+                    .setTitle(request.getTitle())
+                    .setBody(request.getBody())
+                    .build();
+            builder.setNotification(notification);
+            Map<String, String> data = request.getData();
+            if (data != null && !data.isEmpty()) {
+                builder.putAllData(data);
+            }
+            String messageId = messaging.send(builder.build());
+            return PushNotificationResponse.builder()
+                    .messageId(messageId)
+                    .success(true)
+                    .build();
+        }).onErrorReturn(PushNotificationResponse.builder()
+                .success(false)
+                .errorMessage("Failed to send push notification")
+                .build()).subscribeOn(Schedulers.boundedElastic());
+    }
+}
